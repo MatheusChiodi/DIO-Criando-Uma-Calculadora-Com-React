7cde58131771d4f94397e81d6742b330c9593f31
Matheus Chiodi <matheuschiodi20@gmail.com>
2023-11-02 10:20:28 -0300

    Preparando o Container da Calculadora

---
+++ calculadora/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="bg-[#F8F8F2] w-full h-screen flex justify-center items-center">
+      <div className="w-[380px] h-[350px] bg-[#fff] border-[0.5px] border-[#44475A] rounded-md">
+
+      </div>
+    </div>
+  )
+}
+
+export default App
